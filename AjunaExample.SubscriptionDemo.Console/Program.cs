dc6e80e7b0773dca24cbc4fe93668143a413cd12
Dimitris Paxinos <dpaxinos@gmail.com>
2022-07-07 12:48:18 +0200

    Code Cleanups, added exit option and comments in the console app.

--- AjunaExample.SubscriptionDemo.Console/Program.cs
+++ AjunaExample.SubscriptionDemo.Console/Program.cs
@@ -1,58 +1,74 @@
-﻿using System;
-using System.Net.Http;
-using System.Net.WebSockets;
-using System.Threading;
+﻿using System.Net.WebSockets;
 using Ajuna.ServiceLayer.Model;
 using AjunaExample.SubscriptionDemo.RestClient;
 using AjunaExample.SubscriptionDemo.RestClient.Generated.Clients;
 
-// using AjunaExample.SubscriptionDemo.RestClient;
-// using AjunaExample.SubscriptionDemo.RestClient.Generated.Clients;
-// using AjunaExample.ServiceLayer.Model;
 
 namespace AjunaExample.SubscriptionDemo.Console
 {
     internal class Program
     {
+        public static void Main(string[] args)
+        {
+            // Create BaseSubscriptionClient and connect
+            var subscriptionClient = new BaseSubscriptionClient(new ClientWebSocket());
+            subscriptionClient.ConnectAsync(new Uri($"{GetBaseAddress().Replace("http", "ws")}/ws"), CancellationToken.None)
+                .Wait();
+             
+            // Assign Generic Handler for Storage Change
+            subscriptionClient.OnStorageChange = HandleChange;
+
+            // Create HttpClient
+            var httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(GetBaseAddress())
+            };
+            
+            // Create SystemControllerClient
+            var systemControllerClient = new SystemControllerClient(httpClient, subscriptionClient);
+
+            // Subscribe to Number Changes
+            var subscribeNumberTask = systemControllerClient.SubscribeNumber();
+            subscribeNumberTask.Wait();
+            var subscribedSuccessfully = subscribeNumberTask.Result;
+            
+            // Continue only if Subscription has succeeded
+            if (subscribedSuccessfully)
+            {
+                System.Console.WriteLine("Successfully Subscribed. Now listening for Storage Changes...");
+                System.Console.WriteLine("Press ESC to exit");
+            }
+            else
+            {
+                System.Console.WriteLine("Subscription failed. Exiting...");
+                return;
+            }
+
+            // Keep reading the stream waiting for Storage Changes and exit when the user presses ESCAPE
+            bool keepLooping = true;
+            while (keepLooping)
+            {
+                var receiveTask =  subscriptionClient.ReceiveNextAsync(CancellationToken.None);
+                receiveTask.Wait();
+                
+                 if (System.Console.KeyAvailable && System.Console.ReadKey().Key == ConsoleKey.Escape)
+                {
+                    keepLooping = false;
+                }
+            }
+         
+            // Close Connection 
+            subscriptionClient.CloseAsync(WebSocketCloseStatus.Empty,"",CancellationToken.None).Wait();
+        }
+        
         private static string GetBaseAddress()
         {
             return Environment.GetEnvironmentVariable("AJUNA_SERVICE_ENDPOINT") ?? "http://localhost:61752";
         }
 
-        private static void OnStorageChange(StorageChangeMessage message)
+        private static void HandleChange(StorageChangeMessage message)
         {
             System.Console.WriteLine("New Change: " + message.Data);
-        }
-        
-        public static void Main(string[] args)
-        {
-            var subscriptionClient = new BaseSubscriptionClient(new ClientWebSocket());
-            subscriptionClient.ConnectAsync(new Uri("ws://127.0.0.1:61752/ws"), CancellationToken.None)
-                .Wait();
-             
-            subscriptionClient.OnStorageChange = OnStorageChange;
-            
-
-            var httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri("http://localhost:61752");
-            var systemControllerClient = new SystemControllerClient(httpClient, subscriptionClient);
-
-            var subscribeNumberTask = systemControllerClient.SubscribeNumber();
-
-            subscribeNumberTask.Wait();
-            var subscribedSuccessfully = subscribeNumberTask.Result;
-
-            while (true)
-            {
-                var receiveTask =  subscriptionClient.ReceiveNextAsync(CancellationToken.None);
-            
-                receiveTask.Wait();
-            }
-          
-             
-            System.Console.ReadLine();
-             
-            subscriptionClient.CloseAsync(WebSocketCloseStatus.Empty,"",CancellationToken.None).Wait();
         }
     }
 }
